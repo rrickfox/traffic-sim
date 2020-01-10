3f9d732aa818e2dbc4b37866d5482a13feec5151
funketh <theodor.k.funke@gmail.com>
2020-01-10 20:44:30 +0100

    add TeeSection data type

---
+++ Assets/Scripts/DataTypes/TeeSection.cs
@@ -0,0 +1,10 @@
+namespace DataTypes
+{
+    public class TeeSection
+    {
+        public int Rotation;
+        public CombinedAnchors North;
+        public CombinedAnchors East;
+        public CombinedAnchors South;
+    }
+}

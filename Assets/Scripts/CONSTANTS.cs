2a06968e6a3ecb7713c92a6bdcea04f1fccd3150
Richard Fuchs <Rrichard.Fuchs@gmail.com>
2020-01-12 15:24:38 +0100

    follow Unity Coding Guidelines

--- Assets/Scripts/CONSTANTS.cs
+++ Assets/Scripts/CONSTANTS.cs
@@ -2,17 +2,17 @@
 public class CONSTANTS
 {
     //in seconds
-    private float TimeStep = Time.fixedDeltaTime;
+    private float _TimeStep = Time.fixedDeltaTime;
     //Distance a car travels at 5kmph in TIME_STEP seconds
     //in unity distance units
-    private float DistanceStep = 5 * (1000/3600f) * Time.fixedDeltaTime;
+    private float _DistanceStep = 5 * (1000/3600f) * Time.fixedDeltaTime;
 
     public float TIME_STEP
     {
-        get {return TimeStep;}
+        get {return _TimeStep;}
     }
     public float DISTANCE_STEP
     {
-        get {return DistanceStep;}
+        get {return _DistanceStep;}
     }
 }

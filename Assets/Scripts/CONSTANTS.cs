43a79a519cf8d85730cd355c4100647c30476517
Richard Fuchs <Rrichard.Fuchs@gmail.com>
2020-01-12 14:53:11 +0100

    add Constants

---
+++ Assets/Scripts/CONSTANTS.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+public class CONSTANTS
+{
+    //in seconds
+    public float TIME_STEP = Time.fixedDeltaTime;
+    //Distance a car travels at 5kmph in TIME_STEP seconds
+    //in unity distance units
+    public float DISTANCE_STEP = 5 * (1000/3600f) * Time.fixedDeltaTime;
+}

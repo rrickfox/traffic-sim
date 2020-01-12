9c4296642730be2baffa92990704f9016be7d793
funketh <theodor.k.funke@gmail.com>
2020-01-12 16:27:58 +0100

    change getters into public readonly fields

--- Assets/Scripts/CONSTANTS.cs
+++ Assets/Scripts/CONSTANTS.cs
@@ -2,17 +2,8 @@
 public class CONSTANTS
 {
     // in seconds
-    private float _TimeStep = Time.fixedDeltaTime;
+    public readonly float TIME_STEP = Time.fixedDeltaTime;
     // Distance a car travels at 5kmph in TIME_STEP seconds
     // in unity distance units
-    private float _DistanceStep = 5 * (1000/3600f) * Time.fixedDeltaTime;
-
-    public float TIME_STEP
-    {
-        get {return _TimeStep;}
-    }
-    public float DISTANCE_STEP
-    {
-        get {return _DistanceStep;}
-    }
+    public readonly float DISTANCE_STEP = 5 * (1000/3600f) * Time.fixedDeltaTime;
 }

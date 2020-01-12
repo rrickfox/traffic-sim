c00662bd49cdd4674ae499c2aae7884ea01cfd9b
Richard Fuchs <Rrichard.Fuchs@gmail.com>
2020-01-12 21:31:28 +0100

    Fix Coversin Formula, rename Constants

--- Assets/Scripts/CONSTANTS.cs
+++ Assets/Scripts/CONSTANTS.cs
@@ -2,8 +2,8 @@
 public class CONSTANTS
 {
     // in seconds
-    public readonly float TIME_STEP = Time.fixedDeltaTime;
+    public readonly float TIME_UNIT = Time.fixedDeltaTime;
     // Distance a car travels at 5kmph in TIME_STEP seconds
     // in unity distance units
-    public readonly float DISTANCE_STEP = 5 * (10/36f) * Time.fixedDeltaTime;
+    public readonly float DISTANCE_UNIT = 5 * (10/36f) * Time.fixedDeltaTime;
 }
--- Assets/Scripts/Conversion.cs
+++ Assets/Scripts/Conversion.cs
@@ -11,18 +11,18 @@ public class Conversion
     // convert meters per second to units per Timestep
     public float UnitsPerTimeStepFromMPS(float metersPerSecond)
     {
-        return metersPerSecond * _constants.TIME_STEP / _constants.DISTANCE_STEP;
+        return metersPerSecond * _constants.DISTANCE_UNIT / _constants.TIME_UNIT;
     }
 
     // convert units per Timestep to kilometers per hour
-    public float KilometersPerHourFromUPTS(float unitsPerTimeStep)
+    public float KilometersPerHourFromUPTU(float unitsPerTimeUnit)
     {
-        return MetersPerSecondFromUPTS(unitsPerTimeStep * 3.6f);
+        return MetersPerSecondFromUPTU(unitsPerTimeUnit * 3.6f);
     }
 
     // convert units per Timestep to meters per second
-    public float MetersPerSecondFromUPTS(float unitsPerTimeStep)
+    public float MetersPerSecondFromUPTU(float unitsPerTimeUnit)
     {
-        return unitsPerTimeStep / _constants.TIME_STEP * _constants.DISTANCE_STEP;
+        return unitsPerTimeUnit * _constants.TIME_UNIT / _constants.DISTANCE_UNIT;
     }
 }

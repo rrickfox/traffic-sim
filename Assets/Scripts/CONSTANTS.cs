67947e1099d84349f6ae40abee1032a5df14da47
Richard Fuchs <Rrichard.Fuchs@gmail.com>
2020-01-14 14:56:45 +0100

    Add lane width constant

--- Assets/Scripts/CONSTANTS.cs
+++ Assets/Scripts/CONSTANTS.cs
@@ -6,4 +6,6 @@ public class CONSTANTS
     // Distance a car travels at 5kmph in TIME_STEP seconds
     // in unity distance units
     public readonly float DISTANCE_UNIT = 5 * (10/36f) * Time.fixedDeltaTime;
+    // width of a lane
+    public readonly float LANE_WIDTH = 2.5f;
 }

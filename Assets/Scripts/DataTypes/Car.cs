f7e31c39b90a69c5f08ad4ebf19b86ab1dfe2a6b
Richard Fuchs <Rrichard.Fuchs@gmail.com>
2020-01-14 14:56:28 +0100

    Add direction and constructor for Car

--- Assets/Scripts/DataTypes/Car.cs
+++ Assets/Scripts/DataTypes/Car.cs
@@ -6,5 +6,16 @@ namespace DataTypes
         public Road road;
         public float positionOnRoad;
         public float lane;
+        // true if from start to finish
+        public bool direction;
+
+        public Car(int setId, Road setRoad, float setPositionOnRoad, float setLane, bool setDirection)
+        {
+            id = setId;
+            road = setRoad;
+            positionOnRoad = setPositionOnRoad;
+            lane = setLane;
+            direction = setDirection;
+        }
     }
 }

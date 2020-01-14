45bed8ab011570b1825f6d5d81752d8d0ac938a1
Richard Fuchs <Rrichard.Fuchs@gmail.com>
2020-01-14 14:33:59 +0100

    Remove rotation

--- Assets/Scripts/DataTypes/TeeSection.cs
+++ Assets/Scripts/DataTypes/TeeSection.cs
@@ -2,7 +2,6 @@ namespace DataTypes
 {
     public class TeeSection
     {
-        public int rotation;
         public CombinedAnchors north;
         public CombinedAnchors east;
         public CombinedAnchors south;

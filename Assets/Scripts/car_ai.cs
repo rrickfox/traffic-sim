330525739999c3ee1055ca2dcc6f8897bcd83cb2
oh-trentmann <oh.trentmann@gmail.com>
2020-01-16 18:32:55 +0100

    car_ai.cs erstellt+move_Funktion+acceleate_Funktion

---
+++ Assets/Scripts/car_ai.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+float speed = 0f; 
+
+public class car_ai : MonoBehaviour
+ // Längeneinheiten pro Zeiteinheit
+
+
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void fixedUpdate()
+    {
+        move();
+    }
+
+
+    void move()
+    {
+        
+    }
+
+    void accelerate(float acceleation)
+    {
+        speed = speed + acceleation;
+    } 
+}

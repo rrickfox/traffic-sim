6a9b7cc7114454df1c04923ac836cfcb5e06af72
rrickfox <Rrichard.Fuchs@gmail.com>
2020-01-16 16:21:35 +0100

    Change Road constructor to nodes

--- Assets/Scripts/DataTypes/Road.cs
+++ Assets/Scripts/DataTypes/Road.cs
@@ -14,14 +14,14 @@ namespace DataTypes
 
         private CONSTANTS _constants = new CONSTANTS();
 
-        public Road(int id, Vector2 position1, Vector2 position2, int lanes1To2, int lanes2To1)
+        public Road(int id, Node node1, Node node2, int lanes1To2, int lanes2To1)
         {
             this.id = id;
-            node1 = new Node(position1, lanes1To2, lanes2To1);
-            node2 = new Node(position2, lanes2To1, lanes1To2);
+            this.node1 = node1;
+            this.node2 = node2;
             this.lanes1To2 = lanes1To2;
             this.lanes2To1 = lanes2To1;
-            length = Vector2.Distance(position1, position2) / _constants.DISTANCE_UNIT;
+            length = Vector2.Distance(node1.position, node2.position) / _constants.DISTANCE_UNIT;
         }
     }
 }

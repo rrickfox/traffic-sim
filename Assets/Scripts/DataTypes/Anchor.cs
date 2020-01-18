ca62e35de9ea69bcc30c49a1b636e677c872d1bc
funketh <theodor.k.funke@gmail.com>
2020-01-18 21:59:32 +0100

    add `parentRoad` field to Anchor

--- Assets/Scripts/DataTypes/Anchor.cs
+++ Assets/Scripts/DataTypes/Anchor.cs
@@ -4,12 +4,14 @@ namespace DataTypes
 {
     public struct Anchor
     {
+        public Road parentRoad;
         public Vector2 position;
         public int startingLanes;
         public int endingLanes;
         
-        public Anchor(Vector2 position, int startingLanes, int endingLanes)
+        public Anchor(Road parentRoad, Vector2 position, int startingLanes, int endingLanes)
         {
+            this.parentRoad = parentRoad;
             this.position = position;
             this.startingLanes = startingLanes;
             this.endingLanes = endingLanes;

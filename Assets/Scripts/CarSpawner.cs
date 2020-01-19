187d4050674c8258919ae68d7bfdb5b24f8f01b3
rrickfox <Rrichard.Fuchs@gmail.com>
2020-01-19 14:19:48 +0100

    Fix directional changes to offset and position of car spawn

--- Assets/Scripts/CarSpawner.cs
+++ Assets/Scripts/CarSpawner.cs
@@ -19,17 +19,22 @@ class CarSpawner : ScriptableObject
 
     public void displayCar(Car car)
     {
-        Vector2 position = Vector2.Lerp(car.road.node1.position, car.road.node2.position, car.positionOnRoad/car.road.length);
-        Vector3 spawnPoint = new Vector3(position.x, _roadPrefab.transform.localScale.y / 2 + _carPrefab.transform.localScale.y / 2, position.y);
+        Vector2 position = Vector2.zero;
         float angle = 0;
-        float offset = (car.road.lanes1To2 - 1) * CONSTANTS.LANE_WIDTH * car.lane / 2f; // TODO: Check if different for other direction
+        float offset = 0;
         if(car.direction == Direction.direction1To2)
         {
+            position = car.road.node1.position;
             angle = _roads[car.road.id].transform.rotation.y;
+            offset = (((car.road.lanes1To2 + car.road.lanes2To1) / 2) - car.road.lanes1To2 + 0.5f + car.lane) * CONSTANTS.LANE_WIDTH ;
         } else
         {
+            position = car.road.node2.position;
             angle = (_roads[car.road.id].transform.rotation.y + 180) % 360;
+            offset = (((car.road.lanes1To2 + car.road.lanes2To1) / 2) - car.road.lanes2To1 + 0.5f + car.lane) * CONSTANTS.LANE_WIDTH ;
         }
+
+        Vector3 spawnPoint = new Vector3(position.x, _roadPrefab.transform.localScale.y / 2 + _carPrefab.transform.localScale.y / 2, position.y);
         Quaternion rotation = Quaternion.Euler(0, angle, 0);
         GameObject tempCar = Instantiate(_carPrefab, spawnPoint, rotation);
         tempCar.transform.position += tempCar.transform.right * offset;

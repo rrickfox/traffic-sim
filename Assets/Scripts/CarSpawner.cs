0836bd2f8a81ba5c36d72f6b6672daeb1a5d6bf9
rrickfox <Rrichard.Fuchs@gmail.com>
2020-01-19 20:24:53 +0100

    use GetPosition to calculate Position of car on road

--- Assets/Scripts/CarSpawner.cs
+++ Assets/Scripts/CarSpawner.cs
@@ -17,19 +17,14 @@ class CarSpawner : ScriptableObject
 
     public void displayCar(Car car)
     {
-        Vector2 position = Vector2.zero;
+        Vector2 position = car.road.GetPosition(car.positionOnRoad, car.direction, car.lane);
         float angle = 0;
-        float offset = 0;
         if(car.direction == Direction.direction1To2)
         {
-            position = car.road.node1.position;
             angle = RoadAngle(car.road);
-            offset = (((car.road.lanes1To2 + car.road.lanes2To1) / 2) - car.road.lanes1To2 + 0.5f + car.lane) * CONSTANTS.LANE_WIDTH;
         } else
         {
-            position = car.road.node2.position;
             angle = (RoadAngle(car.road) + 180) % 360;
-            offset = (((car.road.lanes1To2 + car.road.lanes2To1) / 2) - car.road.lanes2To1 + 0.5f + car.lane) * CONSTANTS.LANE_WIDTH;
         }
 
         Vector3 spawnPoint = new Vector3(position.x, _roadPrefab.transform.localScale.y / 2 + _carPrefab.transform.localScale.y / 2, position.y);

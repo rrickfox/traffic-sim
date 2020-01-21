b658a39f157fa74093a141ebec86d58908132cf3
rrickfox <Rrichard.Fuchs@gmail.com>
2020-01-21 15:56:59 +0100

    add comments on Road.GetPosition, fix offset bug

--- Assets/Scripts/DataTypes/Road.cs
+++ Assets/Scripts/DataTypes/Road.cs
@@ -24,29 +24,32 @@ namespace DataTypes
             length = Vector2.Distance(node1.position, node2.position) / CONSTANTS.DISTANCE_UNIT;
         }
 
+        // returns position of car on Road, when given progress on Road, driving direction and lane
         public Vector2 GetPosition(float positionOnRoad, Direction direction, float lane)
         {
             Vector2 position = new Vector2();
-            Vector2 forward = new Vector2();
+            Vector2 backwards = new Vector2();
             float offset = 0;
             if(direction == Direction.direction1To2)
             {
+                // get linear position, ignoring lane position and driving direction
                 position = Vector2.Lerp(node1.position, node2.position, positionOnRoad);
-                //set offset to accomodate different lanes
+                // set offset to accomodate different lanes and driving direction
                 offset = (((lanes1To2 + lanes2To1) / 2) - lanes1To2 + 0.5f + lane) * CONSTANTS.LANE_WIDTH;
+                // has to be backwards, because Vector2.Perpendicular returns vector turned counter-clockwise
+                backwards = (node2.position - node1.position).normalized * -1;
 
-                forward = (node2.position - node1.position) / (node2.position - node1.position).magnitude;
-
-                position += forward * offset;
+                // add offset to the right to position
+                position += Vector2.Perpendicular(backwards) * offset; 
             }
             else
             {
                 position = Vector2.Lerp(node2.position, node1.position, positionOnRoad);
                 offset = (((lanes1To2 + lanes2To1) / 2) - lanes2To1 + 0.5f + lane) * CONSTANTS.LANE_WIDTH;
 
-                forward = (node1.position - node2.position) / (node1.position - node2.position).magnitude;
+                backwards = (node1.position - node2.position).normalized * -1;
                 
-                position += forward * offset;
+                position += Vector2.Perpendicular(backwards) * offset;
             }
 
             return position;

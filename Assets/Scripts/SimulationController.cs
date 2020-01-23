052010089931ed77682d701e7197522493c69389
Cold_Blooded <neorobert123@gmx.de>
2020-01-23 22:26:49 +0100

    adapt Simulationcontroler to new Datastructure

--- Assets/Scripts/SimulationController.cs
+++ Assets/Scripts/SimulationController.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using System.Collections.Generic;
 using DataTypes;
+using System.Collections.Immutable;
+using System.Linq;
 public class SimulationController : MonoBehaviour
 {
     public GameObject roadPrefab;
@@ -16,18 +18,39 @@ public class SimulationController : MonoBehaviour
      {
         _roadSpawner = new RoadSpawner(roadPrefab);
 
-         Vector2 pos1 = new Vector2(-140, 0);
-         Vector2 pos2 = new Vector2(140, 0);
-         createRoad(pos1, pos2, 1, 0);
+        // Point1, Point2
+        Point point1 = new Point(new Vector2(-140, 0));
+        Point point2 = new Point(new Vector2(140, 0));
+
+        // Definition lanes1To2
+        HashSet<LaneType> lane1To2_0_types = new HashSet<LaneType>(); 
+        lane1To2_0_types.Add(LaneType.Through);
+        Lane lane1To2_0 = new Lane(lane1To2_0_types);
+
+        IEnumerable<Lane> lanes1To2 = new List<Lane>()
+        {
+            lane1To2_0
+        }.AsEnumerable();
+
+        //lane2To1
+        HashSet<LaneType> lane2To1_0_types = new HashSet<LaneType>();
+        lane1To2_0_types.Add(LaneType.Through);
+        Lane lane2To1_0 = new Lane(lane2To1_0_types);
+
+        IEnumerable<Lane> lanes2To1 = new List<Lane>()
+        {
+            lane2To1_0
+        }.AsEnumerable();
+
+        // Road create..
+        createRoad(point1, point2, lanes1To2, lanes2To1);
      }
 
-     public void createRoad(Vector2 pos1, Vector2 pos2, int lanes1To2, int lanes2To1)
+     public void createRoad(Point point1, Point point2, IEnumerable<Lane> lanes1To2, IEnumerable<Lane> lanes2To1)
      {
-         Node node1 = new Node(pos1, lanes1To2, lanes2To1);
-         Node node2 = new Node(pos2, lanes2To1, lanes1To2);
-         Road tempRoad = new Road(_idRoad, node1, node2, lanes1To2, lanes2To1);
-         _idRoad++;
-         _roads.Add(tempRoad);
-         _roadSpawner.displayRoad(tempRoad);
+        Road tempRoad = new Road(_idRoad, new RoadShape(), point1, point2, lanes2To1, lanes1To2);
+        _idRoad++;
+        _roads.Add(tempRoad);
+        _roadSpawner.displayRoad(tempRoad);
      }
 }

02ad5366f66aac76c25a18f59f8677d2713635b4
oh-trentmann <oh.trentmann@gmail.com>
2020-01-23 19:55:19 +0100

    car_ai in Car umgelagert, states(je Abstad zu anderen) hinzugefügt

--- Assets/Scripts/DataTypes/Car.cs
+++ Assets/Scripts/DataTypes/Car.cs
@@ -1,5 +1,7 @@
 namespace DataTypes
 {
+    public enum CarState{free, reaktion, action};
+
     public class Car
     {
         public readonly int id;
@@ -8,6 +10,7 @@ namespace DataTypes
         public float lane;
         // true if from start to finish
         public Direction direction;
+        public CarState state;
 
         public Car(int id, Road road, float positionOnRoad, float lane, Direction direction)
         {
@@ -16,6 +19,33 @@ namespace DataTypes
             this.positionOnRoad = positionOnRoad;
             this.lane = lane;
             this.direction = direction;
+        }
+
+        public float speed = 0f; // Längeneinheiten pro Zeiteinheit
+        
+        public void Move()
+        {
+            positionOnRoad += speed;
+            switch(state)
+            {
+            case CarState.free:
+                {
+                    
+                }
+                case CarState.reaction:
+                {
+                    
+                }
+                case CarState.action:
+                {
+                
+                }
+
+            }
+        }
+        public void Accelerate(float acceleation)
+        {
+            speed += acceleation;
         }
     }
 }

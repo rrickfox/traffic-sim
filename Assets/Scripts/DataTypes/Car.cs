773415c654e1da890f01f9a4b05459d0e3de62d5
oh-trentmann <oh.trentmann@gmail.com>
2020-01-26 16:37:12 +0100

    Car auf Edge angepasst

--- Assets/Scripts/DataTypes/Car.cs
+++ Assets/Scripts/DataTypes/Car.cs
@@ -1,47 +1,24 @@
 namespace DataTypes
-{
-    public enum CarState{free, reaktion, action};
-
+{ 
     public class Car
     {
-        public readonly int id;
         public Edge road;
         public float positionOnRoad;
         public float lane;
-        // true if from start to finish
-        public Direction direction;
-        public CarState state;
 
-        public Car(int id, Edge road, float positionOnRoad, float lane, Direction direction)
+
+        public Car(Edge road, float positionOnRoad, float lane)
         {
-            this.id = id;
             this.road = road;
             this.positionOnRoad = positionOnRoad;
             this.lane = lane;
-            this.direction = direction;
         }
 
-        public float speed = 0f; // Längeneinheiten pro Zeiteinheit
+        public float speed = 5f; // Längeneinheiten pro Zeiteinheit
         
         public void Move()
         {
-            positionOnRoad += speed;
-            switch(state)
-            {
-            case CarState.free:
-                {
-                    
-                }
-                case CarState.reaction:
-                {
-                    
-                }
-                case CarState.action:
-                {
-                
-                }
-
-            }
+            positionOnRoad += speed; 
         }
         public void Accelerate(float acceleation)
         {

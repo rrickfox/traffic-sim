34996053c5c9b066c9147724ab7e2a25cf07609c
rrickfox <Rrichard.Fuchs@gmail.com>
2020-01-27 23:28:53 +0100

    add spawnCars method (spawn a car if intervall is reached)

--- Assets/Scripts/DataTypes/Vertices/EndPoint.cs
+++ Assets/Scripts/DataTypes/Vertices/EndPoint.cs
@@ -7,17 +7,29 @@ namespace DataTypes
     {
         protected Edge edge;
         private CarSpawner _spawner;
+        // ticks before a car spawns on a lane (index)
+        private float[] _spawnFrequency;
+        // counter for ticks since start
+        private int _ticks = 0;
 
-        public EndPoint(Edge edge, GameObject carPrefab, GameObject roadPrefab)
+        public EndPoint(Edge edge, GameObject carPrefab, GameObject roadPrefab, float[] spawnFrequency)
             : base(ImmutableArray.Create(edge))
         {
             this.edge = edge;
             _spawner = new CarSpawner(carPrefab, roadPrefab);
+            _spawnFrequency = spawnFrequency;
         }
 
-        public void spawnCars(Edge road)
+        public void spawnCars()
         {
-                        
+            for(int i = 0; i < edge.outgoingLanes.Count; i++)
+            {
+                if(_ticks % _spawnFrequency[i] == 0)
+                {
+                    createCar(i);
+                }
+            }
+            _ticks++;
         }
 
         public void createCar(float lane)

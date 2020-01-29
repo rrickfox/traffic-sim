235945c876f9003c889ff8bb362919adcc19a6bc
1NG3M4R <50843146+1NG3M4R@users.noreply.github.com>
2020-01-29 20:40:08 +0100

    Update CarDespawner.cs
    add CarDespawner

--- Assets/Scripts/CarDespawner.cs
+++ Assets/Scripts/CarDespawner.cs
@@ -2,6 +2,28 @@ using UnityEngine;
 using System.Collections.Generic;
 using DataTypes;
 
-class CarSpawner : ScriptableObject {
-  
+class CarDepawner : ScriptableObject 
+{
+    
+    public Edge road;
+    public CarDepawner (Edge road) 
+    {
+
+
+
+        this.road = road;
+    }
+
+    public void removeCars () 
+    {
+        foreach(Car car in road.cars) 
+        {
+            if (car.positionOnRoad > road.length)
+            {
+                Object.Destroy(car.carTransform.gameObject);
+                road.cars.Remove(car);
+            }
+                
+        }
+    }
 }

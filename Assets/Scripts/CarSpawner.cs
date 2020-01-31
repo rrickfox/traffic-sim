b6718564a2d5e7c4cb3b0643dfe3bedfbcff97da
funketh <theodor.k.funke@gmail.com>
2020-01-31 12:08:06 +0100

    refactor spawning of cars from Endpoint into CarSpawner

--- Assets/Scripts/CarSpawner.cs
+++ Assets/Scripts/CarSpawner.cs
@@ -5,13 +5,33 @@ class CarSpawner
 {
     private GameObject _carPrefab;
     private GameObject _roadPrefab;
+    // the edge on which to spawn new cars
+    private Edge _edge;
+    // ticks before a car spawns on a lane (index)
+    private int[] _spawnFrequencies;
+    // counter for ticks since start
+    private int _ticks = 0;
 
-    public CarSpawner (GameObject carPrefab, GameObject roadPrefab)
+    public CarSpawner(GameObject carPrefab, GameObject roadPrefab, Edge edge, int[] spawnFrequencies)
     {
         _carPrefab = carPrefab;
         _roadPrefab = roadPrefab;
+        _edge = edge;
+        _spawnFrequencies = spawnFrequencies;
     }
 
+    public void SpawnCars()
+    {
+        for(var lane = 0; lane < _edge.outgoingLanes.Count; lane++)
+        {
+            if(_ticks % _spawnFrequencies[lane] == 0)
+            {
+                CreateCar(_edge, 0, lane);
+            }
+        }
+        _ticks++;
+    }
+    
     public void CreateCar(Edge road, float positionOnRoad, float lane)
     {
         // construct car
--- Assets/Scripts/DataTypes/Vertices/EndPoint.cs
+++ Assets/Scripts/DataTypes/Vertices/EndPoint.cs
@@ -5,33 +5,19 @@ namespace DataTypes
 {
     class EndPoint : Vertex
     {
-        private Edge _edge;
         private CarSpawner _spawner;
         private CarDespawner _despawner;
-        // ticks before a car spawns on a lane (index)
-        private int[] _spawnFrequencies;
-        // counter for ticks since start
-        private int _ticks = 0;
 
         public EndPoint(Edge edge, GameObject carPrefab, GameObject roadPrefab, int[] spawnFrequencies)
             : base(ImmutableArray.Create(edge))
         {
-            _edge = edge;
-            _spawner = new CarSpawner(carPrefab, roadPrefab);
+            _spawner = new CarSpawner(carPrefab, roadPrefab, edge, spawnFrequencies);
             _despawner = new CarDespawner(edge.other);
-            _spawnFrequencies = spawnFrequencies;
         }
 
         public void SpawnCars()
         {
-            for(var lane = 0; lane < _edge.outgoingLanes.Count; lane++)
-            {
-                if(_ticks % _spawnFrequencies[lane] == 0)
-                {
-                    _spawner.CreateCar(_edge, 0, lane);
-                }
-            }
-            _ticks++;
+            _spawner.SpawnCars();
         }
 
         public void DespawnCars()

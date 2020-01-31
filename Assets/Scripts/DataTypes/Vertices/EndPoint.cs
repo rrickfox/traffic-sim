0953b8329be57824652f2e6b46113fae622a1c8c
funketh <theodor.k.funke@gmail.com>
2020-01-31 12:22:04 +0100

    merge CarSpawner and CarDespawner into EndPoint

--- Assets/Scripts/DataTypes/Vertices/EndPoint.cs
+++ Assets/Scripts/DataTypes/Vertices/EndPoint.cs
@@ -5,24 +5,59 @@ namespace DataTypes
 {
     class EndPoint : Vertex
     {
-        private CarSpawner _spawner;
-        private CarDespawner _despawner;
+        private Edge _edge;
+        private GameObject _carPrefab;
+        private GameObject _roadPrefab;
+        // ticks before a car spawns on a lane (index)
+        private int[] _spawnFrequencies;
+        // counter for ticks since start
+        private int _ticks = 0;
 
         public EndPoint(Edge edge, GameObject carPrefab, GameObject roadPrefab, int[] spawnFrequencies)
             : base(ImmutableArray.Create(edge))
         {
-            _spawner = new CarSpawner(carPrefab, roadPrefab, edge, spawnFrequencies);
-            _despawner = new CarDespawner(edge.other);
+            _edge = edge;
+            _carPrefab = carPrefab;
+            _roadPrefab = roadPrefab;
+            _spawnFrequencies = spawnFrequencies;
         }
 
         public void SpawnCars()
         {
-            _spawner.SpawnCars();
+            for(var lane = 0; lane < _edge.outgoingLanes.Count; lane++)
+            {
+                if(_ticks % _spawnFrequencies[lane] == 0)
+                {
+                    CreateCar(lane);
+                }
+            }
+            _ticks++;
+        }
+    
+        private void CreateCar(float lane)
+        {
+            // construct car
+            var car = new Car(_edge, 0, lane);
+        
+            // display the car graphically
+            var position = _edge.GetAbsolutePosition(car.positionOnRoad, lane);
+            var spawnPoint = new Vector3(position.x, _roadPrefab.transform.localScale.y / 2 + _carPrefab.transform.localScale.y / 2, position.y);
+            var rotation = Quaternion.Euler(0, _edge.angle, 0);
+            var carGameObject = Object.Instantiate(_carPrefab, spawnPoint, rotation);
+            carGameObject.name = "Car_" + CarId.id;
+            car.carTransform = carGameObject.transform;
         }
 
         public void DespawnCars()
         {
-            _despawner.RemoveCars();
+            foreach(var car in _edge.cars) 
+            {
+                if (car.positionOnRoad > _edge.length)
+                {
+                    Object.Destroy(car.carTransform.gameObject);
+                    _edge.cars.Remove(car);
+                }
+            }
         }
     }
 }

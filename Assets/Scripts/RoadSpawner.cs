0c082d54cadf86371c8bf6a0afbca81c107edf6c
rrickfox <Rrichard.Fuchs@gmail.com>
2020-02-04 00:04:40 +0100

    adjust roadSpawner to include curvature of road

--- Assets/Scripts/RoadSpawner.cs
+++ Assets/Scripts/RoadSpawner.cs
@@ -16,19 +16,23 @@ public class RoadSpawner
     public Edge CreateRoad(RoadShape shape, List<Lane> lanes1To2, List<Lane> lanes2To1)
     {
         var road = new Edge(shape, lanes1To2, lanes2To1);
-        // Road length
-        var scaleLength = Vector2.Distance(road.originPoint.position, road.other.originPoint.position); 
-        // Road width
-        var scaleWidth = (road.outgoingLanes.Count + road.incomingLanes.Count) * CONSTANTS.LANE_WIDTH;
-        // 
-        var middlePoint = (road.other.originPoint.position - road.originPoint.position) * 0.5f + road.originPoint.position;
-        // Road spawnpoint
-        var spawnPoint = new Vector3(middlePoint.x, 0, middlePoint.y);
-        var rotation = Quaternion.Euler(0, Vector2.SignedAngle(road.other.originPoint.position - road.originPoint.position, Vector2.right), 0);
         
-        var roadVisual = Object.Instantiate(_roadPrefab, spawnPoint, rotation);
-        roadVisual.transform.localScale = new Vector3(scaleLength, roadVisual.transform.localScale.y, scaleWidth);
-        roadVisual.name = "Road_" + _idRoad;
+        var roadVisual = new GameObject("Road_" + _idRoad);
+        
+        for(int i = 0; i < road.shape.points.Length; i++)
+        {
+            var roadPoint = road.shape.points[i];
+            var spawnPoint = new Vector3(roadPoint.position.x, 0.025f, roadPoint.position.y);
+            var rotation = Quaternion.Euler(0, Vector2.SignedAngle(roadPoint.forward, Vector2.right), 0);
+
+            var roadSegment = Object.Instantiate(_roadPrefab, spawnPoint, rotation);
+            roadSegment.transform.parent = roadVisual.transform;
+            roadSegment.name = "Road_" + _idRoad + "_Segment_" + i;
+
+            var scaleWidth = (road.outgoingLanes.Count + road.incomingLanes.Count) * CONSTANTS.LANE_WIDTH;
+
+            roadSegment.transform.localScale = new Vector3(roadSegment.transform.localScale.x, roadSegment.transform.localScale.y, scaleWidth);
+        }
         
         _idRoad++;
         _roads.Add(roadVisual);

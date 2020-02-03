974bcc893514f139f948629ed2a744fa2800554e
rrickfox <Rrichard.Fuchs@gmail.com>
2020-02-03 16:26:29 +0100

    rename Edge.position to Edge.originPoint

--- Assets/Scripts/DataTypes/Edges/Edge.cs
+++ Assets/Scripts/DataTypes/Edges/Edge.cs
@@ -14,42 +14,41 @@ namespace DataTypes
         public List<Car> cars { get; } = new List<Car>();
         public RoadShape shape { get; }
         // the coordinates of the end of the road from which you look at the road
-        public Vector2 position { get; }
+        public Vector2 originPoint => shape.points[0].position;
         public List<Lane> outgoingLanes { get; }
         // the incomingLanes of this are just the outgoingLanes of the other view
         public List<Lane> incomingLanes => other.outgoingLanes;
         public float length { get; }
         public float angle { get; }
 
-        public Edge(RoadShape shape, Vector2 position, Vector2 otherPosition,
+        public Edge(RoadShape shape, Vector2 otherPosition,
             List<Lane> outgoingLanes, List<Lane> incomingLanes)
         {
             this.shape = shape;
-            this.position = position;
             this.outgoingLanes = outgoingLanes;
-            length = Vector2.Distance(position, otherPosition) / CONSTANTS.DISTANCE_UNIT;
-            angle = Vector2.SignedAngle(otherPosition - position, Vector2.right);
-            other = new Edge(this, otherPosition, incomingLanes);
+            length = Vector2.Distance(originPoint, otherPosition) / CONSTANTS.DISTANCE_UNIT;
+            angle = Vector2.SignedAngle(otherPosition - originPoint, Vector2.right);
+            other = new Edge(this, incomingLanes);
         }
 
         // construct an Edge where other is already constructed
-        private Edge(Edge other, Vector2 position, List<Lane> outgoingLanes)
+        private Edge(Edge other, List<Lane> outgoingLanes)
         {
             this.other = other;
-            this.position = position;
             this.outgoingLanes = outgoingLanes;
+            this.shape = other.shape.Inverse();
             length = this.other.length;
             angle = this.other.angle;
         }
         
         public Vector2 GetAbsolutePosition(float positionOnRoad, float lane)
         {
-            var absolutePosition = Vector2.Lerp(this.position, other.position, positionOnRoad / length);
+            var absolutePosition = Vector2.Lerp(this.originPoint, other.originPoint, positionOnRoad / length);
             // set offset to the right to accomodate different lanes
             var perpandicularOffset = (((this.outgoingLanes.Count + other.outgoingLanes.Count) / 2) - this.outgoingLanes.Count + 0.5f + lane) * CONSTANTS.LANE_WIDTH;
 
             // calculate backwards vector to rotate to right facing vector using Vector2.Perpendicular()
-            var inverse = (other.position - this.position).normalized * -1;
+            var inverse = (other.originPoint - this.originPoint).normalized * -1;
 
             absolutePosition += Vector2.Perpendicular(inverse) * perpandicularOffset;
 

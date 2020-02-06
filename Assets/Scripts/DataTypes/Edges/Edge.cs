2f53a3a4efb1d88955503e2774ff7208ecdaea52
JP <jeanpasqual2@gmail.com>
2020-02-06 18:33:48 +0100

    make one big mesh for the road instead of million cubes and apply texture

--- Assets/Scripts/DataTypes/Edges/Edge.cs
+++ Assets/Scripts/DataTypes/Edges/Edge.cs
@@ -43,7 +43,54 @@ namespace DataTypes
 
         private void Display()
         {
-            for(int i = 0; i < shape.points.Length; i++)
+            var vertices = new Vector3[shape.points.Length * 2];
+            var uvs = new Vector2[vertices.Length];
+            var triangles = new int[2 * (shape.points.Length - 1) * 3];
+            var vertexIndex = 0;
+            var triangleIndex = 0;
+
+            for (int i = 0; i < shape.points.Length; i++)
+            {
+                var p = shape.points[i];
+                var left = new Vector2(-p.forward.y, p.forward.x);
+                var newPosLeft = p.position + left * CONSTANTS.LANE_WIDTH * incomingLanes.Count;
+                var newPosRight = p.position - left * CONSTANTS.LANE_WIDTH * outgoingLanes.Count;
+                vertices[vertexIndex] = new Vector3(newPosLeft.x, 0.025f, newPosLeft.y);
+                vertices[vertexIndex + 1] = new Vector3(newPosRight.x, 0.025f, newPosRight.y);
+
+                var relativPos = i / (float)(shape.points.Length - 1);
+                uvs[vertexIndex] = new Vector2(0f, relativPos);
+                uvs[vertexIndex + 1] = new Vector2(1f, relativPos);
+
+                if (i < shape.points.Length - 1)
+                {
+                    triangles[triangleIndex] = vertexIndex;
+                    triangles[triangleIndex + 1] = vertexIndex + 2;
+                    triangles[triangleIndex + 2] = vertexIndex + 1;
+
+                    triangles[triangleIndex + 3] = vertexIndex + 1;
+                    triangles[triangleIndex + 4] = vertexIndex + 2;
+                    triangles[triangleIndex + 5] = vertexIndex + 3;
+                }
+
+                vertexIndex += 2;
+                triangleIndex += 6;
+            }
+
+            var mesh = new Mesh
+            {
+                vertices = vertices,
+                triangles = triangles,
+                uv = uvs
+            };
+
+            var road = Object.Instantiate(prefab, Vector3.zero, Quaternion.identity);
+            road.GetComponent<MeshFilter>().mesh = mesh;
+            var tiling = shape.length * CONSTANTS.DISTANCE_UNIT * 0.1f;
+            road.GetComponent<MeshRenderer>().sharedMaterial.SetTextureScale("_MainTex", new Vector2(1, tiling));
+
+            /*
+            for (int i = 0; i < shape.points.Length; i++)
             {
                 var roadPoint = shape.points[i];
                 var spawnPoint = new Vector3(roadPoint.position.x, 0.025f, roadPoint.position.y);
@@ -56,7 +103,7 @@ namespace DataTypes
                 var scaleWidth = (outgoingLanes.Count + incomingLanes.Count) * CONSTANTS.LANE_WIDTH;
 
                 roadSegment.transform.localScale = new Vector3(roadSegment.transform.localScale.x, roadSegment.transform.localScale.y, scaleWidth);
-            }
+            }*/
         }
     }
 
--- Assets/Scripts/PointPlacer.cs
+++ Assets/Scripts/PointPlacer.cs
@@ -5,6 +5,8 @@ using Utility;
 
 public class PointPlacer : MonoBehaviour
 {
+    public Material material;
+
     void Start()
     {
         var p0_s = new Vector2(-140, -70);
@@ -19,12 +21,52 @@ public class PointPlacer : MonoBehaviour
         var curve_1 = new BezierCurve(p1_s, p1_c, p1_e);
         var shape = new RoadShape(new List<BezierCurve> {curve_0, curve_1});
 
-        foreach(var p in shape.points)
+        var vertices = new Vector3[shape.points.Length * 2];
+        var uvs = new Vector2[vertices.Length];
+        var triangles = new int[2 * (shape.points.Length - 1) * 3];
+        var vertexIndex = 0;
+        var triangleIndex = 0;
+
+        for (int i = 0; i < shape.points.Length; i++)
         {
-            var g = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            g.transform.position = new Vector3(p.position.x, 0, p.position.y);
-            g.transform.rotation = Quaternion.Euler(0, Vector2.SignedAngle(p.forward, Vector2.right), 0);
-            g.transform.localScale = new Vector3(CONSTANTS.DISTANCE_UNIT, 0.1f, CONSTANTS.LANE_WIDTH);
+            var p = shape.points[i];
+            var left = new Vector2(-p.forward.y, p.forward.x);
+            var newPosLeft = p.position + left * CONSTANTS.LANE_WIDTH;
+            var newPosRight = p.position - left * CONSTANTS.LANE_WIDTH;
+            vertices[vertexIndex] = new Vector3(newPosLeft.x, 0.025f, newPosLeft.y);
+            vertices[vertexIndex + 1] = new Vector3(newPosRight.x, 0.025f, newPosRight.y);
+
+            var relativPos = i / (float)(shape.points.Length-1);
+            uvs[vertexIndex] = new Vector2(0f, relativPos);
+            uvs[vertexIndex + 1] = new Vector2(1f, relativPos);
+
+            if (i < shape.points.Length-1)
+            {
+                triangles[triangleIndex] = vertexIndex;
+                triangles[triangleIndex + 1] = vertexIndex + 2;
+                triangles[triangleIndex + 2] = vertexIndex + 1;
+
+                triangles[triangleIndex + 3] = vertexIndex + 1;
+                triangles[triangleIndex + 4] = vertexIndex + 2;
+                triangles[triangleIndex + 5] = vertexIndex + 3;
+            }
+
+            vertexIndex += 2;
+            triangleIndex += 6;
         }
+
+        var mesh = new Mesh
+        {
+            vertices = vertices,
+            triangles = triangles,
+            uv = uvs
+        };
+
+        var g = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        g.GetComponent<MeshFilter>().mesh = mesh;
+        var mr = g.GetComponent<MeshRenderer>();
+        mr.sharedMaterial = material;
+        var tiling = shape.length * CONSTANTS.DISTANCE_UNIT * 0.1f;
+        mr.sharedMaterial.SetTextureScale("_MainTex", new Vector2(1, tiling));
     }
 }

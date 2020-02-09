dcd8b93aa143f8e85c30a0e1b0758825a39106a6
Konstantin <konsti.bobenko@gmail.com>
2020-02-09 17:55:42 +0100

    add button functionality
    - simulation start button
    - options button
        - options menu with volume slider (does not do anything yet)
    - quit button

---
+++ Assets/Scripts/Interface/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public void StartSimulation()
+	{
+		// load simulation
+		SceneManager.LoadSceneAsync(1);
+	}
+	
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}

a6ab962fe5b24240f43f85662fd95742fcba6fbe
rrickfox <Rrichard.Fuchs@gmail.com>
2020-02-14 09:21:07 +0100

    add update publisher

---
+++ Assets/Scripts/UpdatePublisher.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdatePublisher : MonoBehaviour
+{
+    public static event System.Action PreFixedUpdate;
+    public static event System.Action NormalFixedUpdate;
+    public static event System.Action PostFixedUpdate;
+    void FixedUpdate()
+    {
+        PreFixedUpdate();
+        NormalFixedUpdate();
+        PostFixedUpdate();
+    }
+
+    public static void SubscribePreFixedUpdate(System.Action subscriber)
+    {
+        PreFixedUpdate += subscriber;
+    }
+
+    public static void SubscribeNormalFixedUpdate(System.Action subscriber)
+    {
+        NormalFixedUpdate += subscriber;
+    }
+
+    public static void SubscribePostFixedUpdate(System.Action subscriber)
+    {
+        PostFixedUpdate += subscriber;
+    }
+
+    public static void UnsubscribePreFixedUpdate(System.Action subscriber)
+    {
+        PreFixedUpdate -= subscriber;
+    }
+
+    public static void UnsubscribeNormalFixedUpdate(System.Action subscriber)
+    {
+        NormalFixedUpdate -= subscriber;
+    }
+
+    public static void UnsubscribePostFixedUpdate(System.Action subscriber)
+    {
+        PostFixedUpdate -= subscriber;
+    }
+}

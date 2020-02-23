e3b8a1c32f7b95ccadb0a7e82cf66a2bfccec017
Ingmar <anubarak@Ingmars-iMac.fritz.box>
2020-02-23 16:53:49 +0100

    Update roundabout.cs

--- Assets/Scripts/roundabout.cs
+++ Assets/Scripts/roundabout.cs
@@ -4,4 +4,11 @@ using System.Linq;
 using DataTypes;
 using static Pathfinding.Pathfinding;
 
-//erzeugt eine spezielle T-kreuzug, aus der Kreisverkehre gebaut werden können
+//erzeugt eine spezielle T-kreuzug ("protoRoundabout"), aus der Kreisverkehre gebaut werden können. Da ein Kreisverkehr nur in eine Richtung befahrbar ist, bedeutet das für die T-kreuzung, dass eine Straße beliebeige Gestalt haben darf, die anderen beiden jedoch Einbahnstraßen sein müssen (eine von der Kreuzung weg, die andere zur Kreuzung hin)
+
+public class protoRoundabout : MonoBehaviour
+{
+    public GameObject roadPrefab;
+    public GameObject carPrefab;
+
+}

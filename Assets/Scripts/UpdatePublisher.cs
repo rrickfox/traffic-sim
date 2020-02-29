884bffe8b3c4b27a2e7d1b93d409d353e102031f
funketh <theodor.k.funke@gmail.com>
2020-02-29 22:15:19 +0100

    shorten code using expression bodies

--- Assets/Scripts/UpdatePublisher.cs
+++ Assets/Scripts/UpdatePublisher.cs
@@ -5,6 +5,7 @@ public class UpdatePublisher : MonoBehaviour
     public static event System.Action PreFixedUpdate;
     public static event System.Action NormalFixedUpdate;
     public static event System.Action PostFixedUpdate;
+    
     void FixedUpdate()
     {
         PreFixedUpdate?.Invoke();
@@ -12,33 +13,11 @@ public class UpdatePublisher : MonoBehaviour
         PostFixedUpdate?.Invoke();
     }
 
-    public static void SubscribePreFixedUpdate(System.Action subscriber)
-    {
-        PreFixedUpdate += subscriber;
-    }
+    public static void SubscribePreFixedUpdate(System.Action subscriber) => PreFixedUpdate += subscriber;
+    public static void SubscribeNormalFixedUpdate(System.Action subscriber) => NormalFixedUpdate += subscriber;
+    public static void SubscribePostFixedUpdate(System.Action subscriber) => PostFixedUpdate += subscriber;
 
-    public static void SubscribeNormalFixedUpdate(System.Action subscriber)
-    {
-        NormalFixedUpdate += subscriber;
-    }
-
-    public static void SubscribePostFixedUpdate(System.Action subscriber)
-    {
-        PostFixedUpdate += subscriber;
-    }
-
-    public static void UnsubscribePreFixedUpdate(System.Action subscriber)
-    {
-        PreFixedUpdate -= subscriber;
-    }
-
-    public static void UnsubscribeNormalFixedUpdate(System.Action subscriber)
-    {
-        NormalFixedUpdate -= subscriber;
-    }
-
-    public static void UnsubscribePostFixedUpdate(System.Action subscriber)
-    {
-        PostFixedUpdate -= subscriber;
-    }
+    public static void UnsubscribePreFixedUpdate(System.Action subscriber) => PreFixedUpdate -= subscriber;
+    public static void UnsubscribeNormalFixedUpdate(System.Action subscriber) => NormalFixedUpdate -= subscriber;
+    public static void UnsubscribePostFixedUpdate(System.Action subscriber) => PostFixedUpdate -= subscriber;
 }

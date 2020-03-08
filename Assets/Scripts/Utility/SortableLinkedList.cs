ea9a2c6c4ab00d0aa5d4c9433de2e084502cf58a
funketh <theodor.k.funke@gmail.com>
2020-03-08 22:32:47 +0100

    fix many operations in SortableLinkedList

--- Assets/Scripts/Utility/SortableLinkedList.cs
+++ Assets/Scripts/Utility/SortableLinkedList.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Utility
 {
@@ -52,23 +53,13 @@ namespace Utility
                 node.next = first;
             }
             else
+            {
                 last = node;
+                node.next = null;
+            }
 
             first = node;
-        }
-
-        // Runtime: O(1)
-        private static void InsertBefore(TNode before, TNode toInsert)
-        {
-            var after = before.previous;
-            if (after != null)
-            {
-                after.next = toInsert;
-                toInsert.previous = after;
-            }
-            
-            before.previous = toInsert;
-            toInsert.next = before;
+            node.previous = null;
         }
 
         // Runtime: O(1)
@@ -76,11 +67,41 @@ namespace Utility
         {
             if (node.previous != null)
                 node.previous.next = node.next;
-            
+            else
+                first = (TNode) node.next;
+
             if (node.next != null)
                 node.next.previous = node.previous;
+            else
+                last = (TNode) node.previous;
         }
-        
+
+        // Runtime: O(1)
+        private void MoveBefore(TNode toMove, TNode before)
+        {
+            if (before == toMove) return;
+            
+            Remove(toMove);
+            InsertBefore(toMove, before);
+        }
+
+        // Runtime: O(1)
+        private void InsertBefore(TNode toInsert, TNode before)
+        {
+            // update toInsert
+            toInsert.previous = before.previous;
+            toInsert.next = before;
+            
+            // update node that will be behind toInsert
+            if (before.previous != null)
+                before.previous.next = toInsert;
+            else
+                first = toInsert;
+            
+            // update node that will be in front of toInsert
+            before.previous = toInsert;
+        }
+
         /* Implemented using insertion sort because:
                - other sorting algorithms are inefficient on linked lists (except for merge sort)
                - insertion sort works well on already (almost) sorted lists
@@ -90,6 +111,9 @@ namespace Utility
          */
         public void Sort()
         {
+            // list is empty
+            if (first == null) return;
+            
             var node = (TNode) first.next;
             while (node != null)
             {
@@ -100,8 +124,7 @@ namespace Utility
                 
                 var nextNode = (TNode) node.next;
                 // move node into its correct position in the sorted part of the list
-                if (insertBefore != node)
-                    InsertBefore(node, insertBefore);
+                MoveBefore(node, insertBefore);
                 node = nextNode;
             }
         }
@@ -133,21 +156,8 @@ namespace Utility
         // Get the nodes in the list which are greater than the specified node.
         // This is implemented as efficiently as possible since the next element can always be accessed immediately.
         public IEnumerable<TNode> AllGreater(TNode node)
-        {
-            var restIsGreater = false;
-            foreach (var otherNode in LookForward(node))
-            {
-                if (restIsGreater)
-                    yield return otherNode;
-                else
-                {
-                    restIsGreater = _comparer.Compare(node, otherNode) < 0;
-                    if (restIsGreater)
-                        yield return otherNode;
-                }
-            }
-        }
-        
+            => LookForward(node).Where(otherNode => _comparer.Compare(node, otherNode) < 0);
+
         // Get the nodes in the list which are greater than or equal to the specified node.
         // This is implemented as efficiently as possible since the next element can always be accessed immediately.
         public IEnumerable<TNode> AllGreaterOrEqual(TNode node)

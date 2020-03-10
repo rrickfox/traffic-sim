3ba7446a14e97ce5abe64bbfeadafcd676315c40
funketh <theodor.k.funke@gmail.com>
2020-03-10 18:28:47 +0100

    refactor normal driving behaviour into own class

--- Assets/Scripts/DataTypes/Car.cs
+++ Assets/Scripts/DataTypes/Car.cs
@@ -12,19 +12,19 @@ namespace DataTypes
 {
     public class Car : GameObjectData, ISortableListNode
     {
-        // https://de.wikipedia.org/wiki/Gr%C3%B6%C3%9Fenordnung_(Beschleunigung)
-        private static Acceleration _MAX_ACCELERATION { get; } = Acceleration.FromMetersPerSecondSquared(4f);
-        private static Acceleration _MAX_BRAKING_DECELERATION { get; } = - Acceleration.FromMetersPerSecondSquared(10f);
-        private static Length _BUFFER_DISTANCE { get; } = Length.FromMeters(50f);
-        
         public static TypePublisher typePublisher { get; } = new TypePublisher();
         
         public ISortableListNode previous { get; set; }
         public ISortableListNode next { get; set; }
-
+        
         public ITrack track => segment.track;
         public List<RouteSegment> route { get; }
         public RouteSegment segment { get; private set; }
+        
+        // https://de.wikipedia.org/wiki/Gr%C3%B6%C3%9Fenordnung_(Beschleunigung)
+        public Acceleration maxAcceleration { get; } = Acceleration.FromMetersPerSecondSquared(4f);
+        public Acceleration maxBrakingDeceleration { get; } = - Acceleration.FromMetersPerSecondSquared(10f);
+        public Length bufferDistance { get; } = Length.FromMeters(50f);
 
         public Length positionOnRoad { get; private set; } = Length.Zero;
         public float lane { get; private set; }
@@ -51,76 +51,44 @@ namespace DataTypes
             _publisher.Subscribe(Drive);
         }
 
-        public void Drive()
+        private void Drive()
+        {
+            DetermineState();
+            var driver = SelectDriver();
+            acceleration = driver.GetAcceleration();
+            ExecuteAcceleration();
+        }
+
+        private void DetermineState()
         {
             // TODO: figure out what state the car is in
             _state = CarState.DriveNormally;
-            
-            switch (_state)
-            {
-                case CarState.DriveNormally: DriveNormally(); break;
-                case CarState.WantToChangeLane: throw new NotImplementedException();
-            }
-            
-            Move();
         }
 
-        private void DriveNormally()
+        private IDriver SelectDriver()
         {
-            var frontCar = GetFrontCar();
-            SimulateHumanness();
-            Accelerate(GetAcceleration(frontCar));
+            switch (_state)
+            {
+                case CarState.DriveNormally:
+                    return new NormalCarState(this, GetFrontCar());
+                case CarState.WantToChangeLane:
+                    throw new NotImplementedException();
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
-        
-        // Accelerate in Units per Timestep
-        private void Accelerate(Acceleration acceleration)
-            => speed += acceleration.Times(TimeSpan.FromSeconds(1));
 
         // Returns the Car in front of the current Car
         private Car GetFrontCar()
+            => track.cars.AllGreater(this).FirstOrDefault(other => other.lane == lane);
+
+        private void ExecuteAcceleration()
         {
-            return track.cars.AllGreater(this).FirstOrDefault(other => other.lane == lane);
-        }
-
-        //Returns the Time to Collision
-        private TimeSpan GetCollisionTime(Car frontCar)
-            => (frontCar.positionOnRoad - positionOnRoad - CAR_LENGTH).DividedBy(speed - frontCar.speed);
-
-        private Length GetMinimumDistance(Car frontCar)
-            => 3/2 * (speed.Squared() - frontCar.speed.Squared()).DividedBy(_MAX_ACCELERATION);
-
-        private Acceleration GetAcceleration(Car frontCar)
-        {
-            if (frontCar == null)
-                return _MAX_ACCELERATION;
-
-            // TODO
-            if (frontCar.positionOnRoad == positionOnRoad)
-                return Random.value * _MAX_ACCELERATION;
-
-            var minimumDistance = GetMinimumDistance(frontCar);
-            var collisionTime = GetCollisionTime(frontCar);
-            var frontDistance = frontCar.positionOnRoad - positionOnRoad;
-            var computedAcceleration = frontCar.acceleration
-                + 2 * (frontDistance - minimumDistance) / collisionTime / collisionTime
-                + 2 * (frontCar.speed - speed) / collisionTime;
-            return Formulas.Min(computedAcceleration, _MAX_ACCELERATION);
-        }
-
-        // Add an aspect of randomness to the car's behaviour
-        private void SimulateHumanness()
-        {
-            if (Random.value * 1000000 < 1)
-                Accelerate(Acceleration.FromMetersPerSecondSquared(-speed.MetersPerSecond / 3));
-        }
-
-        private void Move()
-        {
+            speed += acceleration.Times(TimeSpan.FromSeconds(1));
             positionOnRoad += speed * TimeSpan.FromSeconds(1);
-            var roadPoint = track.GetAbsolutePosition(positionOnRoad, lane);
-            transform.position = new Vector3(roadPoint.position.x, transform.localScale.y / 2 + ROAD_HEIGHT, roadPoint.position.y);
-
+            
             UpdatePosition();
+            
             // if car is at end of RouteSegment, get next routeSegment if there is one
             if(positionOnRoad >= track.length && route.Count > 0)
             {
@@ -136,6 +104,59 @@ namespace DataTypes
             var roadPoint = track.GetAbsolutePosition(positionOnRoad, lane);
             transform.position = new Vector3(roadPoint.position.x, transform.localScale.y / 2 + ROAD_HEIGHT, roadPoint.position.y);
             transform.rotation = Quaternion.Euler(0, Vector2.SignedAngle(roadPoint.forward, Vector2.right), 0);
+        }
+    }
+
+    public interface IDriver
+    {
+        Acceleration GetAcceleration();
+    }
+    
+    public struct NormalCarState : IDriver
+    {
+        private Car _myCar { get; }
+        private Car _frontCar { get; }
+        private Acceleration _acceleration { get; set; }
+
+        public NormalCarState(Car myCar, Car frontCar)
+        {
+            _myCar = myCar;
+            _frontCar = frontCar;
+            _acceleration = Acceleration.Zero;
+        }
+
+        public Acceleration GetAcceleration()
+        {
+            SimulateHumanness();
+            
+            if (_frontCar == null)
+                return _myCar.maxAcceleration;
+
+            // TODO
+            if (_frontCar.positionOnRoad == _myCar.positionOnRoad)
+                return Random.value * _myCar.maxAcceleration;
+
+            var minimumDistance = GetMinimumDistance();
+            var collisionTime = GetCollisionTime();
+            var frontDistance = _frontCar.positionOnRoad - _myCar.positionOnRoad;
+            var computedAcceleration = _frontCar.acceleration
+                                       + 2 * (frontDistance - minimumDistance) / collisionTime / collisionTime
+                                       + 2 * (_frontCar.speed - _myCar.speed) / collisionTime;
+            return Formulas.Min(computedAcceleration, _myCar.maxAcceleration);
+        }
+
+        // The Time to Collision
+        private TimeSpan GetCollisionTime()
+            => (_frontCar.positionOnRoad - _myCar.positionOnRoad - CAR_LENGTH).DividedBy(_myCar.speed - _frontCar.speed);
+
+        private Length GetMinimumDistance()
+            => 1.5 * (_myCar.speed.Squared() - _frontCar.speed.Squared()).DividedBy(_myCar.maxAcceleration);
+
+        // Add an aspect of randomness to the car's behaviour
+        private void SimulateHumanness()
+        {
+            if (Random.value * 1000000 < 1)
+                _acceleration -= Acceleration.FromMetersPerSecondSquared(_myCar.speed.MetersPerSecond / 3);
         }
     }
 }

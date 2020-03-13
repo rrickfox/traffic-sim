4cfb3975a8b18b865835d500c7bd5b50f94c532c
JP <jeanpasqual2@gmail.com>
2020-03-13 22:09:49 +0100

    update Play/Pause Button, add color palette

---
+++ Assets/Scripts/Interface/UIElements/ButtonToggle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Interface {
+    public class ButtonToggle : MonoBehaviour
+    {
+        public GameObject playImage;
+        public GameObject pauseImage;
+
+        public void ToggleState()
+        {
+            SimulationManager.pause = !SimulationManager.pause;
+            playImage.SetActive(SimulationManager.pause);
+            pauseImage.SetActive(!SimulationManager.pause);
+        }
+    }
+}

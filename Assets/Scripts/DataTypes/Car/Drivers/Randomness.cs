82fefdc7fd96022abb6fd3539434e6f940001028
funketh <theodor.k.funke@gmail.com>
2020-03-15 18:11:09 +0100

    increase effect of humanness but don't apply it to slow cars

--- Assets/Scripts/DataTypes/Car/Drivers/Randomness.cs
+++ Assets/Scripts/DataTypes/Car/Drivers/Randomness.cs
@@ -6,9 +6,10 @@ namespace DataTypes.Drivers
     public static class Randomness
     {
         // Add an aspect of randomness to the car's behaviour
-        public static Acceleration SimulateHumanness(Car myCar, double chance = .1, double factor = .3)
+        public static Acceleration SimulateHumanness(Car myCar, double chance = .1, double factor = 2)
         {
-            if (Random.RANDOM.NextDouble() > chance)
+            var alreadyTooSlow = myCar.speed < .5 * myCar.track.speedLimit;
+            if (alreadyTooSlow || Random.RANDOM.NextDouble() > chance)
                 return Acceleration.Zero;
             
             var lowerBound = Acceleration.FromMetersPerSecondSquared(- factor * myCar.speed.MetersPerSecond);

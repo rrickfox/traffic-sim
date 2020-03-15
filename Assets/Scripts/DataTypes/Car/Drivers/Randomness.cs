93c3d196cabe757339c6d895e64d3b6ead099d70
funketh <theodor.k.funke@gmail.com>
2020-03-15 17:54:39 +0100

    adjust humanness simulation

--- Assets/Scripts/DataTypes/Car/Drivers/Randomness.cs
+++ Assets/Scripts/DataTypes/Car/Drivers/Randomness.cs
@@ -1,14 +1,18 @@
 using UnitsNet;
-using UnityEngine;
+using Utility;
 
 namespace DataTypes.Drivers
 {
     public static class Randomness
     {
         // Add an aspect of randomness to the car's behaviour
-        public static Acceleration SimulateHumanness(Car myCar)
-            => Random.value * 1000000 < 1
-                ? -Acceleration.FromMetersPerSecondSquared(myCar.speed.MetersPerSecond / 3)
-                : Acceleration.Zero;
+        public static Acceleration SimulateHumanness(Car myCar, double chance = .1, double factor = .3)
+        {
+            if (Random.RANDOM.NextDouble() > chance)
+                return Acceleration.Zero;
+            
+            var lowerBound = Acceleration.FromMetersPerSecondSquared(- factor * myCar.speed.MetersPerSecond);
+            return Random.RANDOM.NextDouble() * lowerBound;
+        }
     }
 }

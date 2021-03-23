db0d59ea4809b2438fd8cc8d4ef7d7e36cdd9bae
rrickfox <Rrichard.Fuchs@gmail.com>
2021-03-23 22:42:45 +0100

    Add Comment, adjust spawning frequency to something more observable

--- Assets/Scripts/DataTypes/Car/Drivers/Randomness.cs
+++ Assets/Scripts/DataTypes/Car/Drivers/Randomness.cs
@@ -8,6 +8,7 @@ namespace DataTypes.Drivers
         // Add an aspect of randomness to the car's behaviour
         public static Acceleration SimulateHumanness(Car myCar, double chance = .1, double factor = 2)
         {
+            // if car is going less than half of the speed limit or in (default) 90% of the cases, do not adjust the acceleration
             var alreadyTooSlow = myCar.speed < .5 * myCar.track.speedLimit;
             if (alreadyTooSlow || Random.RANDOM.NextDouble() > chance)
                 return Acceleration.Zero;

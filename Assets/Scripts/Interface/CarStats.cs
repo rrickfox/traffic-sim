f8176f74205206d6b94b0edd06ba87f08a8ba5df
Richard Fuchs <31240615+rrickfox@users.noreply.github.com>
2022-02-01 15:09:26 +0100

    Check every .25 seconds if followed car has changed, update car stats accordingly

--- Assets/Scripts/Interface/CarStats.cs
+++ Assets/Scripts/Interface/CarStats.cs
@@ -14,6 +14,7 @@ namespace Interface
         private FreeCamera _freeCam;
         private Car _car;
         private bool _visible = false;
+        private float _timeSinceCarCheck = 0;
 
         // Start is called before the first frame update
         private void Start()
@@ -33,6 +34,11 @@ namespace Interface
                     panel.SetActive(true);
                     _car = (Car) _freeCam._targetCar.GetComponent<LinkedBehaviour>().data;
                 }
+                if (_timeSinceCarCheck > .25)
+                {
+                    _car = (Car) _freeCam._targetCar.GetComponent<LinkedBehaviour>().data;
+                    _timeSinceCarCheck = 0;
+                }
             }
             else
             {
@@ -49,6 +55,7 @@ namespace Interface
                     distance -= frontCar.length / 2;
                 frontDistanceText.text = Mathf.Round((float) (distance - _car.positionOnRoad - (_car.length / 2)).Meters * 10f) / 10f + " m";
             }
+            _timeSinceCarCheck += Time.deltaTime;
         }
     }
 }
